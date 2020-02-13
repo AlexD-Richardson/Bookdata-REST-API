7e40db68a6816d1a739df1b2da72d134dcef855a
Alex Richardson <richardson.d.alex@gmail.com>
2020-02-12 22:29:41 -0500

    Added different cases for different methods

--- main.go
+++ main.go
@@ -8,9 +8,24 @@ import (
 type server struct{}
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"message": "hello world"}`))
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "Get got gotten"}`))
+	case "POST":
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message": "Post got posted"}`))
+	case "PUT":
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message": "Put got put in it's place"}`))
+	case "DELETE":
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message": "Delete didn't get deleted"}`))
+	default:
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message": "Method not found. What are you doing?"}`))
+	}
 }
 
 func main() {
